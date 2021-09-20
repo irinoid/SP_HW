56a3d0dc41a04ca1fc098b21e534afe3bd10af2b
irinoid <iryna.kapelka@gmail.com>
2021-09-20 20:20:42 +0300

    убрала пробелы

--- src/lesson4/Lesson4.java
+++ src/lesson4/Lesson4.java
@@ -5,39 +5,29 @@ public class Lesson4 {
         //1
         int i = 1;
         while (i <= 10) {
-
             System.out.print(i + " ");
             i++;
         }
         System.out.println();
         for (i = i - 1; i > 0; i--) {
-
             System.out.print(i + " ");
         }
         System.out.println();
         //2
-
         int firstFridayDay = 6;
         for (int a = firstFridayDay; a <= 31; a += 7) {
-
-
             System.out.println("Сегодня пятница, " + a + "-ое число. Необходимо подготовить отчет.");
         }
-//3
+        //3
         int currYear = 2021;
         int lastYear = currYear - 200;
         int futureYear = currYear + 100;
-
-
         for (i = lastYear; i < futureYear; i++) {
             if (i % 79 == 0) {
-
                 System.out.println(i);
             }
         }
-
         //4
-
         for (i = 1; i <= 30; i++) {
             System.out.print(i + ":");
             if (i % 3 == 0) {
